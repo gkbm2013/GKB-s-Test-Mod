6ce37bcc40adc7b100500cdc2d7534f275ef85a1
GKB <gkbm2013@gmail.com>
2018-07-04 17:44:00 +0800

    ignore idea folder

--- src/main/java/tinker_io/TinkerIO.java
+++ src/main/java/tinker_io/TinkerIO.java
@@ -16,7 +16,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import tinker_io.proxy.CommonProxy;
 import tinker_io.registry.BlockRegistry;
 import tinker_io.registry.ItemRegistry;
-import tinker_io.registry.RecipeRegistry;
 
 @Mod(modid = TinkerIO.MOD_ID,
         version = TinkerIO.VERSION,
@@ -55,7 +54,7 @@ public class TinkerIO {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        RecipeRegistry.init();
+
     }
 
     @Mod.EventHandler
