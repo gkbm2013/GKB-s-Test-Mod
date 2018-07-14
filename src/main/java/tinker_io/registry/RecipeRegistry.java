b726db4a42370b736c467687b9c621cd0c3637c4
Li-Xian Chen <twbrandon7@gmail.com>
2018-07-14 14:00:27 +0800

    Add smelting recipe

--- src/main/java/tinker_io/registry/RecipeRegistry.java
+++ src/main/java/tinker_io/registry/RecipeRegistry.java
@@ -1,4 +1,10 @@
 package tinker_io.registry;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class RecipeRegistry {
+    public static void init(){
+        GameRegistry.addSmelting(BlockRegistry.testBlock, new ItemStack(ItemRegistry.testItem), 0.7f);
+    }
 }
